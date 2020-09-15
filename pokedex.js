3bcda2d6ef15d9f3e5e224a7836c375d11269fd3
Bardyy <battar@uwo.ca>
2020-09-15 15:46:59 -0400

    Search Bar Functionalities.
    Programmed the search bars to sort and find each pokemon by using their letters or numbers.

--- pokedex.js
+++ pokedex.js
@@ -1,18 +1,74 @@
-function PokedexFunction() {
 
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("searchBar");
-    filter = input.value.toUpperCase();
-    ul = document.getElementsByClassName("myMenu");
-    li = ul.getElementsByTagName("li");
+const Pokemons = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise',
+    'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 'Raticate'];
 
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
+document.getElementById('search').addEventListener('input', (e) => {
+
+    let pokemonArray = [];
+    if (e.target.value) {
+        pokemonArray = Pokemons.filter(Pokemon => Pokemon.toLowerCase().includes(e.target.value));
+        pokemonArray = pokemonArray.map(Pokemon => '<li>' + Pokemon + '</li>');
+
     }
+    showPokemonArray(pokemonArray);
+
+});
+
+
+function showPokemonArray(pokemonArray) {
+    const html = !pokemonArray.length ? '' : pokemonArray.join('');
+    (document.querySelector('ul').innerHTML = html);
 }
+
+
+function numberFinder() {
+    var numberinput = document.getElementById("search-2").value;
+    switch (numberinput) {
+
+        case "1": alert("The Pokemon is Bulbasaur");
+            break;
+        case "2": alert("The Pokemon is Ivysaur");
+            break;
+        case "3": alert("The Pokemon is venusaur");
+            break;
+        case "4": alert("The Pokemon is Charmander");
+            break;
+        case "5": alert("The Pokemon is Charmeleon");
+            break;
+        case "6": alert("The Pokemon is Charizard");
+            break;
+        case "7": alert("The Pokemon is Squirtle");
+            break;
+        case "8": alert("The Pokemon is Wartortle");
+            break;
+        case "9": alert("The Pokemon is Blastoise");
+            break;
+        case "10": alert("The Pokemon is Caterpie");
+            break;
+        case "11": alert("The Pokemon is Metapod");
+            break;
+        case "12": alert("The Pokemon is Butterfree");
+            break;
+        case "13": alert("The Pokemon is Weedle");
+            break;
+        case "14": alert("The Pokemon is Kakuna");
+            break;
+        case "15": alert("The Pokemon is Beedrill");
+            break;
+        case "16": alert("The Pokemon is Pidgey");
+            break;
+        case "17": alert("The Pokemon is Pidgeotto");
+            break;
+        case "18": alert("The Pokemon is Pidgeot");
+            break;
+        case "19": alert("The Pokemon is Rattata");
+            break;
+        case "20": alert("The Pokemon is Raticate");
+            break;
+
+    }
+
+
+
+}
+
